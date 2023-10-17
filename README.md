ff3c965f87fe24962f612f0becb7856a09a0942e
Woojoung <chae3229@gmail.com>
2023-10-18 03:30:12 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Race-Condition-Counter
+Race Condition Counter in Java
+
+
+### 🖥️ 프로젝트 소개
+Race Condition 콘솔 어플리케이션.
+<br>
+
+### ⚙️ 개발 환경
+- `Java 8`
+- `JDK 1.8.0`
---
+++ src/Counter.java
@@ -0,0 +1,40 @@
+public class Counter implements Runnable {
+    private int c = 0;
+
+    public void increment() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            //Auto-generated catch block
+            e.printStackTrace();
+        }
+        c++;
+    }
+
+    public void decrement() {
+        c--;
+    }
+
+    public int getValue() {
+        return c;
+    }
+
+    @Override
+    public void run() {
+        //incrementing
+        this.increment();
+        System.out.println("Value for Thread After increment " + Thread.currentThread().getName() + " " + this.getValue());
+        //decrementing
+        this.decrement();
+        System.out.println("Value for Thread at last " + Thread.currentThread().getName() + " " + this.getValue());
+
+//        synchronized (this){
+//            //incrementing
+//            this.increment();
+//            System.out.println("Value for Thread After increment " + Thread.currentThread().getName() + " " + this.getValue());
+//            //decrementing
+//            this.decrement();
+//            System.out.println("Value for Thread at last " + Thread.currentThread().getName() + " " + this.getValue());
+//        }
+    }
+}
