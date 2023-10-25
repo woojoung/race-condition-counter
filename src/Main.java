998698ef01b7016dceef594c5d0f33729e7c8d15
woojoung <chae3229@gmail.com>
2023-10-25 18:14:30 +0900

    feat: comment 추가

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         Thread t1 = new Thread(counter, "Thread-1");
         Thread t2 = new Thread(counter, "Thread-2");
         Thread t3 = new Thread(counter, "Thread-3");
+        // thread 3개가 번갈아(context switching) 실헹한다.
+        // thread 실행순서 보장 X
         t1.start();
         t2.start();
         t3.start();
